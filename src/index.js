a650a231f0ef32e1b773fc1efbec93e2a264b3ec
muhyizz <muhyizz.mohammed@gmail.com>
2023-10-19 01:52:50 +0800

    added the edit function

--- src/index.js
+++ src/index.js
@@ -2,50 +2,39 @@ import './styles.css';
 import { Todo } from './todo';
 import { Project } from './project';
 import { Portfolio } from './portfolio';
-import { loadDivPortfolio, populateDivPortfolio, oneAdditionProject } from './portfolioLayout';
-import { loadInputProject, createProject, displayProjectTask, addProjectTask, addTaskLayout } from './projectLayout';
-
-const onLoadRight = () =>{
-    const right = document.getElementById('right');
-    right.innerHTML='';
-    const welcomePage = document.createElement('div');
-    welcomePage.innerText = "Welcome to Amazing List";
-    right.appendChild(welcomePage);
-};
+import { loadDivPortfolio, populateDivPortfolio, oneAdditionProject } from './leftLayout';
+import { loadInputProject, createProject, displayProjectTask, addProjectTask, addTaskLayout, onLoadRight, loadProjectEdit, editProject } from './rightLayout';
 
 let taskOne = new Todo('title','description');
 let taskTwo = new Todo('a','description2');
-let listOne = new Project('My list','c','d','e', [taskOne,taskTwo]);
-let listTwo = new Project('Old list','c','d','e', [taskOne,taskTwo]);
+
+let listOne = new Project('First list','c','d','e', [taskOne,taskTwo]);
+let listTwo = new Project('Second list','c','d','e', [taskOne,taskTwo]);
+let listThree = new Project('Third list','c','d','e', [taskOne,taskTwo]);
+let listFour = new Project('Fourth list','c','d','e', [taskOne,taskTwo]);
 
 listOne.title ='new title';
 
-let task3 = new Todo('title3','description');
+// let task3 = new Todo('title3','description');
 
-listOne.addToDO(task3);
-listOne.delToDo(task3);
+// listOne.addToDO(task3);
+// listOne.delToDo(task3);
 
-let port1 = new Portfolio;
-
-port1.addProject(listOne);
-port1.addProject(listTwo);
-port1.addProject(listOne);
-
-listOne.printToDoDescription()
-
-loadDivPortfolio();
+// listOne.printToDoDescription()
 
 let firstPortfolio = new Portfolio();
 
 firstPortfolio.addProject(listOne);
 firstPortfolio.addProject(listTwo);
-firstPortfolio.addProject(listOne);
-firstPortfolio.addProject(listTwo);
+firstPortfolio.addProject(listThree);
+firstPortfolio.addProject(listFour);
 
 // Start of Program
+loadDivPortfolio();
 populateDivPortfolio(firstPortfolio)
 onLoadRight();
 let projectTitleLock = '';
+let projectEditLock = 'fail';
 
 
 const addProjectButton = document.getElementById('addProject');
@@ -53,7 +42,35 @@ addProjectButton.addEventListener('click',()=>{
     loadInputProject();
 });
 
+left.addEventListener('click', (event)=>{
+    if(event.target.classList.contains('projectList')){
+        projectTitleLock = event.target.innerHTML;
+        displayProjectTask(event.target.innerHTML,firstPortfolio);
+        
+    }
+
+})
+
+
+
+left.addEventListener('click', (event)=>{
+    if(event.target.classList.contains('editProject')){
+       projectEditLock = event.target.parentElement.previousElementSibling.innerHTML;
+       loadProjectEdit();
+    }
+
+})
+
 const right = document.getElementById('right');
+
+right.addEventListener('click', (event)=>{
+    if(event.target.id === 'editProjectLine'){
+        editProject(event,projectEditLock,firstPortfolio)
+        populateDivPortfolio(firstPortfolio)
+    }
+})
+
+
 
 right.addEventListener('click', (event)=>{
     const target = event.target;
@@ -65,16 +82,12 @@ right.addEventListener('click', (event)=>{
     }
 })
 
-
-left.addEventListener('click', (event)=>{
-    let projectList = document.querySelectorAll('.projectList'); 
-    if(event.target.classList.contains('projectList')){
-        projectTitleLock = event.target.innerHTML;
-        displayProjectTask(event.target.innerHTML,firstPortfolio);
-        
+right.addEventListener('click', (event)=>{
+    if(event.target.id === 'cancelProjectLine'){
+        onLoadRight();
     }
-
 })
+
 
 right.addEventListener('click', (event)=>{
     if(event.target.id === 'addTask'){
@@ -89,3 +102,8 @@ right.addEventListener('click', (event)=>{
     }
 })
 
+right.addEventListener('click', (event)=>{
+    if(event.target.id === 'cancelTask'){
+        displayProjectTask(projectTitleLock,firstPortfolio);
+    }
+})
